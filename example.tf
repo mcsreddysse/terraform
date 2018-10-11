f7e9c86ac5e2381b283ca07f5bf908fabee89fa0
mcsreddysse <33459352+mcsreddysse@users.noreply.github.com>
2018-10-11 17:09:48 +0530

    example.tf
    updated the terraform code in the terraform.tf file

---
+++ example.tf
@@ -0,0 +1,70 @@
+provider "aws" {
+  region = "us-east-2"
+#  access_key = ""
+#  secret_key = ""
+   profile = "${var.profile}"
+}
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+# Create an internet gateway to give our subnet access to the open internet
+resource "aws_internet_gateway" "internet-gateway" {
+  vpc_id = "${aws_vpc.vpc.id}"
+}
+# Give the VPC internet access on its main route table
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.vpc.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.internet-gateway.id}"
+}
+# Create a subnet to launch our instances into
+resource "aws_subnet" "default" {
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags {
+    Name = "Public"
+  }
+}
+# Our default security group to access
+# instances over SSH and HTTP
+resource "aws_security_group" "default" {
+  name        = "terraform_securitygroup"
+  description = "Used for public instances"
+  vpc_id      = "${aws_vpc.vpc.id}"
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+# HTTP access from the VPC
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  instance_type = "t2.micro"
+  ami ="ami-0b59bfac6be064b78"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  # We're going to launch into the public subnet for this.
+  # Normally, in production environments, webservers would be in
+  # private subnets.
+  subnet_id = "${aws_subnet.default.id}"
+  # The connection block tells our provisioner how to
+  # communicate with the instance
+}
+
+
