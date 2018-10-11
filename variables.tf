58ea55bebbc55299055a401f882fc474b1f49ea9
mcsreddysse <33459352+mcsreddysse@users.noreply.github.com>
2018-10-11 17:13:07 +0530

    variables.tf
    updated terraform code in variables.tf file

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  default = "us-east-2"
+}
+variable "profile" {
+  description = "AWS Credentials profile you want to use"
+}
+#variable "key_name" {
+#  description = "Name of the AWS key pair"
+#}
